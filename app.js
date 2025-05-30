235e4d1dfc4e3aebdeddb59266cf1d5813e24988
Erickmau09 <mauerick09@gmail.com>
2025-05-29 21:23:06 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+//Uso de variables
+/*var nombre = 'Erick';
+console.log (nombre);
+console.log (typeof(nombre));
+
+
+var edad = 34;
+console.log (edad);
+console.log (typeof(edad));
+
+edad = 'Cinco';
+console.log (edad);
+console.log (typeof(edad));
+
+var sueldo = 1900.99
+console.log (sueldo);
+console.log (typeof(sueldo));
+
+
+var tieneTrabajo = false;
+console.log (tieneTrabajo);
+console.log (typeof (tieneTrabajo));
+
+
+var puestoDeTrabajo;
+console.log (puestoDeTrabajo);
+puestoDeTrabajo = null;
+console.log (puestoDeTrabajo);
+**********************************************************/
+
+//Operadores matematicos +, -, *, /
+
+var edadAna, edadMaria, diferenciaEdad;
+var sumaEdades, yearAna, yearMaria, yearActual;
+
+edadAna = 34;
+edadMaria = 28;
+yearActual = 2019;
+
+diferenciaEdad = edadAna - edadMaria;
+sumaEdades = edadAna + edadMaria;
+
+yearAna = yearActual - edadAna;
+yearMaria = yearActual - edadMaria;
+
+console.log ('La diferencia de sus edades son: ' + diferenciaEdad + ' años');
+console.log (sumaEdades);
+console.log ('Año en que nació Ana ' + yearAna);
+console.log ('Año en que nació Maria ' + yearMaria);
+console.log (yearActual * 5);
+console.log (yearActual / 2);
+
+
+
